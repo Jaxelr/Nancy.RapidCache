154a9895a814574eca653545e1d309a642b78716
Jaxel Rojas <jaxelr@gmail.com>
2018-04-06 17:39:41 -0400

    Added ProjectionTests class.

---
+++ tests/UnitTests/ProjectionTests.cs
@@ -0,0 +1,28 @@
+﻿using Nancy.RapidCache.Projection;
+using Nancy.RapidCache.Tests.Fakes;
+using System;
+using Xunit;
+
+namespace Nancy.RapidCache.Tests.UnitTests
+{
+    public class ProjectionTests
+    {
+        private DateTime expirationDate = DateTime.Now.AddMinutes(15);
+
+        [Fact]
+        public void Cacheable_response_created()
+        {
+            //Arrange
+            var fakeResponse = new FakeResponse();
+
+            //Act
+            var cacheableResponse = new CacheableResponse(fakeResponse, expirationDate);
+
+            //Assert
+            Assert.NotNull(cacheableResponse);
+            Assert.Equal(fakeResponse.ContentType, cacheableResponse.ContentType);
+            Assert.Equal(fakeResponse.Headers, cacheableResponse.Headers);
+            Assert.Equal(fakeResponse.StatusCode, cacheableResponse.StatusCode);
+        }
+    }
+}
