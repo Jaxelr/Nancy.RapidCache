300256bec65c4137d318b1a955e68136fb06ce7e
Jaxel Rojas <jaxelr@gmail.com>
2018-04-08 09:48:07 -0400

    Added Url Hash Key Generator as optional Key Generator.

---
+++ src/Nancy.RapidCache/CacheKey/UrlHashKeyGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Nancy.RapidCache.CacheKey
+{
+    public class UrlHashKeyGenerator : ICacheKeyGenerator
+    {
+        public string Get(Request request)
+        {
+            var md5 = MD5.Create();
+            byte[] hash = md5.ComputeHash(Encoding.UTF8.GetBytes(request.Url.ToString()));
+            return Convert.ToBase64String(hash);
+        }
+    }
+}
---
+++ tests/UnitTests/UrlHashKeyGeneratorTests.cs
@@ -0,0 +1,41 @@
+﻿using Nancy.RapidCache.CacheKey;
+using Nancy.RapidCache.Tests.Mocks;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Nancy.RapidCache.Tests.UnitTests
+{
+    public class UrlHashKeyGeneratorTests
+    {
+        private const string ACCEPT = "Accept";
+        private const string ACCEPT_VALUE = "text/plain";
+        private const string METHOD = "GET";
+        private const string PATH = "FakierPath";
+        private readonly Dictionary<string, IEnumerable<string>> Acceptheader = new Dictionary<string, IEnumerable<string>>();
+
+        public UrlHashKeyGeneratorTests()
+        {
+            Acceptheader.Add(ACCEPT, new[] { ACCEPT_VALUE });
+        }
+
+        [Fact]
+        public void Keyed_by_url()
+        {
+            var keyGen = new UrlHashKeyGenerator();
+            var request = new FakeRequest(
+                method: METHOD,
+                path: PATH,
+                headers: Acceptheader);
+
+            //Act
+            string key = keyGen.Get(request);
+            byte[] bytekey = System.Convert.FromBase64String(key);
+
+            //Assert
+
+            //Validating lengths from hash created.
+            Assert.Equal(24, key.Length);
+            Assert.Equal(16, bytekey.Length);
+        }
+    }
+}
