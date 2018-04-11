e2a6cc0219d8e10e4c162e19114881136e26b48f
Jaxel Rojas <jaxelr@gmail.com>
2018-04-11 07:19:44 -0400

    Renamed folder of Projection into plural. Separated tests from Extensions and Projections.

--- tests/UnitTests/Extensions.cs
+++ tests/UnitTests/Extensions.cs
@@ -1,6 +1,4 @@
 ﻿using Nancy.RapidCache.Extensions;
-using Nancy.RapidCache.Projection;
-using Nancy.RapidCache.Tests.Extensions;
 using Nancy.RapidCache.Tests.Fakes;
 using Nancy.Responses.Negotiation;
 using System;
@@ -10,27 +8,10 @@ using Xunit;
 
 namespace Nancy.RapidCache.Tests.UnitTests
 {
-    public class Projection
+    public class Extensions
     {
         private DateTime expirationDate = DateTime.Now.AddMinutes(15);
         private string CACHE_KEY = Helper.CacheHeader;
-
-        [Fact]
-        public void Cacheable_response_created()
-        {
-            //Arrange
-            var fakeResponse = new FakeResponse();
-
-            //Act
-            var cacheableResponse = new CacheableResponse(fakeResponse, expirationDate);
-
-            //Assert
-            Assert.NotNull(cacheableResponse);
-            Assert.Equal(fakeResponse.ContentType, cacheableResponse.ContentType);
-            Assert.Equal(fakeResponse.Headers, cacheableResponse.Headers);
-            Assert.Equal(fakeResponse.StatusCode, cacheableResponse.StatusCode);
-            Assert.Equal(fakeResponse.GetContents(), cacheableResponse.GetContents());
-        }
 
         [Fact]
         public void Cacheable_negotiator_created()
