2ffa4f026ebf8617f49a5ad0686d15b4047a9e08
Jaxel Rojas <jaxelr@gmail.com>
2018-04-17 10:33:50 -0400

    Added minor corrections to sample application.

--- samples/ApplicationBootstrapper.cs
+++ samples/ApplicationBootstrapper.cs
@@ -1,22 +1,23 @@
 ﻿using Nancy;
 using Nancy.RapidCache.CacheStore;
-using Nancy.RapidCache.CacheKey;
 using Nancy.RapidCache.Extensions;
 using Nancy.Routing;
 using System;
+using Nancy.TinyIoc;
+using Nancy.Bootstrapper;
 
 namespace Asp.Net.Example
 {
     public class ApplicationBootrapper : DefaultNancyBootstrapper
     {
-        protected override void ApplicationStartup(Nancy.TinyIoc.TinyIoCContainer container, Nancy.Bootstrapper.IPipelines pipelines)
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
             base.ApplicationStartup(container, pipelines);
 
             /*enable RapidCache, vary by url params query, form and accept headers */
             this.EnableRapidCache(container.Resolve<IRouteResolver>(), ApplicationPipelines, new[] { "query", "form", "accept" });
 
-            /* Can also call just with the pipelines and it will equally work since the constructor equivalent is query, form, accept headers */
+            /* Can also call just without keys and it will cache urls based on base urls only. */
             //this.EnableRapidCache(container.Resolve<IRouteResolver>(), ApplicationPipelines);
 
             /* Enable this for all requests to be cached configured */
@@ -31,7 +32,7 @@ namespace Asp.Net.Example
 
         public void ConfigureCache(NancyContext context)
         {
-            if (context.Response.StatusCode == HttpStatusCode.OK)
+            if (context.Response.StatusCode == HttpStatusCode.OK && context.Request.Method == "GET")
             {
                 context.Response = context.Response.AsCacheable(DateTime.UtcNow.AddSeconds(30));
             }
