edc5a1a10f3734183b0c54c77cb683a101cc644a
Jaxel Rojas <jaxelr@gmail.com>
2018-07-29 08:12:39 -0400

    Added CouchbaseCacheStore.

---
+++ src/Nancy.RapidCache.Couchbase/Nancy.RapidCache.Couchbase/CouchbaseCacheStore.cs
@@ -0,0 +1,23 @@
+﻿using Nancy.RapidCache.Projection;
+using System;
+
+namespace Nancy.RapidCache.CacheStore
+{
+    public class CouchbaseCacheStore : ICacheStore
+    {
+        public CachedResponse Get(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Set(string key, NancyContext context, DateTime absoluteExpiration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
