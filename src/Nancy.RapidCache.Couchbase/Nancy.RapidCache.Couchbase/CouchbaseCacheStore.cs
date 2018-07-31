d11ea3ec26081d3d2a95f000b9647f11914e1b34
Jaxel Rojas <jaxelr@gmail.com>
2018-07-31 12:12:15 -0400

    Added default couchbase implementation.

--- src/Nancy.RapidCache.Couchbase/Nancy.RapidCache.Couchbase/CouchbaseCacheStore.cs
+++ src/Nancy.RapidCache.Couchbase/Nancy.RapidCache.Couchbase/CouchbaseCacheStore.cs
@@ -1,23 +1,58 @@
-﻿using Nancy.RapidCache.Projection;
+﻿using Couchbase.Core;
+using Nancy.RapidCache.Projection;
 using System;
 
 namespace Nancy.RapidCache.CacheStore
 {
     public class CouchbaseCacheStore : ICacheStore
     {
+        private readonly ICluster _cluster;
+        private readonly string _bucketname;
+
+        public CouchbaseCacheStore(ICluster cluster, string bucketname)
+        {
+            _cluster = cluster;
+            _bucketname = bucketname;
+        }
+
         public CachedResponse Get(string key)
         {
-            throw new NotImplementedException();
+            using (var bucket = _cluster.OpenBucket(_bucketname))
+            {
+                var result = bucket.Get<SerializableResponse>(key);
+
+                if (result.Success)
+                {
+                    return new CachedResponse(result.Value);
+                }
+            }
+
+            return null;
         }
 
         public void Remove(string key)
         {
-            throw new NotImplementedException();
+            using (var bucket = _cluster.OpenBucket(_bucketname))
+            {
+                bucket.Remove(key);
+            }
         }
 
         public void Set(string key, NancyContext context, DateTime absoluteExpiration)
         {
-            throw new NotImplementedException();
+            int length = (absoluteExpiration - DateTime.UtcNow).Seconds;
+            var span = new TimeSpan(0, 0, length);
+
+            using (var bucket = _cluster.OpenBucket(_bucketname))
+            {
+                var serialize = new SerializableResponse(context.Response, absoluteExpiration);
+                var result = bucket.Upsert(key, serialize, span);
+
+                if (!result.Success)
+                {
+                    throw new Exception($"Could not complete operation of Upsert, using cluster configuration: {_cluster.Configuration}");
+                }
+            }
         }
     }
 }
