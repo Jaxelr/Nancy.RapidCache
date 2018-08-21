0c0f5d51c139f2c846f2bb54a7397e73bae8a178
Jaxel Rojas <jaxelr@gmail.com>
2018-08-21 06:58:52 -0400

    More disk cache tests.
    Also cleaned up cache after certain operation.

--- tests/UnitTests/CacheStores/DiskCacheStoreFixtures.cs
+++ tests/UnitTests/CacheStores/DiskCacheStoreFixtures.cs
@@ -9,9 +9,17 @@ namespace Nancy.RapidCache.Tests.UnitTests.CacheStores
     public class DiskCacheStoreFixtures
     {
         private readonly string Path = @"c:\temp\";
+        private readonly string InvalidPath = @".\\invalid\\path";
+        private TimeSpan TimeSpan => new TimeSpan(0, 0, 10);
         private const string TEST_KEY_1 = "FileRequest1";
         private const string TEST_KEY_2 = "FileRequest2";
         private const string TEST_KEY_3 = "FileRequest3";
+
+        [Fact]
+        public void Disk_cache_invalid_path() => Assert.Throws<ArgumentException>(() => new DiskCacheStore(InvalidPath));
+
+        [Fact]
+        public void Disk_cache_invalid_path_with_timespan() => Assert.Throws<ArgumentException>(() => new DiskCacheStore(InvalidPath, new TimeSpan()));
 
         [Fact]
         public void Disk_cache_empty_get()
@@ -28,6 +36,20 @@ namespace Nancy.RapidCache.Tests.UnitTests.CacheStores
             Assert.Null(response);
         }
 
+        [Fact]
+        public void Disk_cache_empty_get_with_timespan()
+        {
+            //Arrange
+            var cache = new DiskCacheStore(Path, TimeSpan);
+            var context = new NancyContext() { Response = new FakeResponse() { } };
+
+            //Act
+            cache.Set(string.Empty, context, DateTime.UtcNow.AddMinutes(1));
+            var response = cache.Get(string.Empty);
+
+            //Assert
+            Assert.Null(response);
+        }
 
         [Fact]
         public void Disk_cache_set_get()
@@ -40,6 +62,7 @@ namespace Nancy.RapidCache.Tests.UnitTests.CacheStores
             //Act
             cache.Set(TEST_KEY_1, context, expirationDate);
             var response = cache.Get(TEST_KEY_1);
+            cache.Remove(TEST_KEY_1);
 
             //Assert
             Assert.Equal(context.Response.ContentType, response.ContentType);
