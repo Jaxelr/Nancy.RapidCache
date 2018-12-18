b8da0fc933d715f8e2acc73dd8758e51766540d6
Jaxel Rojas <jaxelr@gmail.com>
2018-12-18 07:25:17 -0400

    Update sample application to netcore 2.2

--- samples/Nancy,RapidCache.SampleApplication/Startup.cs
+++ samples/Nancy,RapidCache.SampleApplication/Startup.cs
@@ -3,22 +3,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Nancy.Owin;
+using System;
 
 namespace Asp.Net.Example
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public static IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.AddLogging(opt =>
+            {
+                opt.AddConsole();
+                opt.AddDebug();
+            });
+
+            var provider = services.BuildServiceProvider();
+
+            return provider;
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole();
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
