451c3691156ee2078da9a512dd48247e0473fff9
Jaxel Rojas <jaxelr@gmail.com>
2019-03-02 14:31:31 -0400

    chore: Bump dependencies
    ReportGenerator
    Microsoft.Net.Test.Sdk

--- tools/testcoverage.bat
+++ tools/testcoverage.bat
@@ -9,7 +9,7 @@ mv coverage.xml ../%destination%/coverage.xml
 
 cd ../%destination%
 
-set "reportgenerator=%UserProfile%\.nuget\packages\reportgenerator\4.0.12\tools\net47\ReportGenerator.exe"
+set "reportgenerator=%UserProfile%\.nuget\packages\reportgenerator\4.0.14\tools\net47\ReportGenerator.exe"
 set "targetdir=."
 
 "%reportGenerator%" -reports:coverage.xml -reporttypes:HtmlInline -targetdir:%targetdir%
