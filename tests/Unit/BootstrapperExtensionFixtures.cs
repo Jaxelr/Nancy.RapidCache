c2193bd42474adfc50ec6ebaf0135baa35c94554
Jaxel Rojas <jaxelr@gmail.com>
2019-05-17 06:12:43 -0400

    test: adding bootstrapper extension fixtures
    Signed-off-by: Jaxel Rojas <jaxelr@gmail.com>

---
+++ tests/Unit/BootstrapperExtensionFixtures.cs
@@ -0,0 +1,75 @@
+﻿using Nancy.Bootstrapper;
+using Nancy.RapidCache.Extensions;
+using Nancy.RapidCache.Tests.Fakes;
+using Xunit;
+
+namespace Nancy.RapidCache.Tests.Unit
+{
+    [TestCaseOrderer("Nancy.RapidCache.Tests.AlphabeticalTestOrderer", "Nancy.RapidCache.Tests")]
+    public class BootstrapperExtensionFixtures
+    {
+        [Fact]
+        public void Disable_bootstrapper_with_cache_disable_key()
+        {
+            //Arrange
+            var bootstrapper = new FakeDefaultBootstrapper();
+
+            //Act
+            Assert.Throws<BootstrapperException>(() => bootstrapper.EnableCacheDisableKey(null));
+
+            //Assert
+            Assert.False(RapidCache.IsCacheEnabled());
+            Assert.False(Defaults.DisableCache.Enabled);
+        }
+
+        [Fact]
+        public void Disable_bootstrapper_with_cache_removal_key()
+        {
+            //Arrange
+            var bootstrapper = new FakeDefaultBootstrapper();
+
+            //Act
+            Assert.Throws<BootstrapperException>(() => bootstrapper.EnableCacheDisableKey(null));
+
+            //Assert
+            Assert.False(RapidCache.IsCacheEnabled());
+            Assert.False(Defaults.DisableCache.Enabled);
+        }
+
+        [Theory]
+        [InlineData("CustomRemoval1Key")]
+        public void Enable_bootstrapper_with_cache_removal_key(string mockKey)
+        {
+            //Arrange
+            var bootstrapper = new FakeDefaultBootstrapper();
+            var routeResolver = new FakeRouteResolver();
+
+            //Act
+            RapidCache.Enable(bootstrapper, routeResolver, new FakePipelines());
+            bootstrapper.EnableCacheRemovalKey(mockKey);
+
+            //Assert
+            Assert.True(RapidCache.IsCacheEnabled());
+            Assert.Equal(Defaults.RemoveCache.Key, mockKey);
+            Assert.True(Defaults.RemoveCache.Enabled);
+        }
+
+        [Theory]
+        [InlineData("CustomDisable1Key")]
+        public void Enable_bootstrapper_with_cache_disable_key(string mockKey)
+        {
+            //Arrange
+            var bootstrapper = new FakeDefaultBootstrapper();
+            var routeResolver = new FakeRouteResolver();
+
+            //Act
+            RapidCache.Enable(bootstrapper, routeResolver, new FakePipelines());
+            bootstrapper.EnableCacheDisableKey(mockKey);
+
+            //Assert
+            Assert.True(RapidCache.IsCacheEnabled());
+            Assert.Equal(Defaults.DisableCache.Key, mockKey);
+            Assert.True(Defaults.DisableCache.Enabled);
+        }
+    }
+}
