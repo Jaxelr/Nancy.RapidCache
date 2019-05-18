1aac235c1fe1d2e6a5ad2fe3e7e0f1bf4748480d
Jaxel Rojas <jaxelr@gmail.com>
2019-05-18 07:24:19 -0400

    test: added priority test case orderer
    Signed-off-by: Jaxel Rojas <jaxelr@gmail.com>

---
+++ tests/Attributes/PriorityAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Nancy.RapidCache.Tests
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class PriorityAttribute : Attribute
+    {
+        public PriorityAttribute(int priority)
+        {
+            Priority = priority;
+        }
+
+        public int Priority { get; private set; }
+    }
+}
---
+++ tests/TestCaseOrderers/PriorityOrderer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Nancy.RapidCache.Tests
+{
+    public class PriorityOrderer : ITestCaseOrderer
+    {
+        private const string Argument = "Priority";
+
+        public IEnumerable<TTestCase> OrderTestCases<TTestCase>(IEnumerable<TTestCase> testCases) where TTestCase : ITestCase
+        {
+            var sortedMethods = new SortedDictionary<int, List<TTestCase>>();
+
+            foreach (TTestCase testCase in testCases)
+            {
+                int priority = 0;
+
+                foreach (IAttributeInfo attr in testCase.TestMethod.Method.GetCustomAttributes((typeof(PriorityAttribute).AssemblyQualifiedName)))
+                    priority = attr.GetNamedArgument<int>(Argument);
+
+                GetOrCreate(sortedMethods, priority).Add(testCase);
+            }
+
+            foreach (var list in sortedMethods.Keys.Select(priority => sortedMethods[priority]))
+            {
+                list.Sort((x, y) => StringComparer.OrdinalIgnoreCase.Compare(x.TestMethod.Method.Name, y.TestMethod.Method.Name));
+                foreach (TTestCase testCase in list)
+                    yield return testCase;
+            }
+        }
+
+        private static TValue GetOrCreate<TKey, TValue>(IDictionary<TKey, TValue> dictionary, TKey key) where TValue : new()
+        {
+            if (dictionary.TryGetValue(key, out TValue result)) return result;
+
+            result = new TValue();
+            dictionary[key] = result;
+
+            return result;
+        }
+    }
+}
