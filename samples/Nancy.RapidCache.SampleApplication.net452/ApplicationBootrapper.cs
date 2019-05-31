1cfb729d4173317e76649cb453a3a4b25e8e6e88
Jaxel Rojas <jaxelr@gmail.com>
2019-05-30 23:59:26 -0400

    chore: added sample application targeting net452

---
+++ samples/Nancy.RapidCache.SampleApplication.net452/ApplicationBootrapper.cs
@@ -0,0 +1,47 @@
+﻿using Nancy.Bootstrapper;
+using Nancy.RapidCache.CacheStore;
+using Nancy.RapidCache.Extensions;
+using Nancy.Routing;
+using Nancy.TinyIoc;
+
+namespace Nancy.RapidCache.SampleApplication.Net452
+{
+    public class ApplicationBootrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            /*enable RapidCache, vary by url params query, form and accept headers */
+            this.EnableRapidCache(container.Resolve<IRouteResolver>(), ApplicationPipelines, new[] { "query", "form", "accept" });
+
+            //This option enables the ability to disable the cache per request by adding a query string param that includes ?DisableMeQuery=true
+            this.EnableCacheDisableKey("DisableMeQuery");
+
+            //This option enables the ability to remove keys from the cache per request by adding a query string param that includes ?RemoveMeQuery=true
+            this.EnableCacheRemovalKey("RemoveMeQuery");
+
+            /* Can also call just without keys and it will cache urls based on base urls only. */
+            //this.EnableRapidCache(container.Resolve<IRouteResolver>(), ApplicationPipelines);
+
+            /* Enable this for all requests to be cached configured */
+            //pipelines.AfterRequest.AddItemToStartOfPipeline(ConfigureCache);
+
+            /* Enable cache using the DiskCacheStore, vary by url query, form and accept headers */
+            //this.EnableRapidCache(container.Resolve<IRouteResolver>(), ApplicationPipelines, new[] { "query", "form", "accept" }, new DiskCacheStore("c:/tmp/cache"));
+
+            /* Enable cache using Redis , using the same key variations */
+            //this.EnableRapidCache(container.Resolve<IRouteResolver>(), ApplicationPipelines, new[] { "query", "form", "accept" }, new RedisCacheStore("localhost"));
+        }
+
+        public void ConfigureCache(NancyContext context)
+        {
+            if (context.Response.StatusCode == HttpStatusCode.OK &&
+                (context.Request.Method == "GET" ||
+                context.Request.Method == "HEAD"))
+            {
+                context.Response = context.Response.AsCacheable(System.DateTime.UtcNow.AddSeconds(30));
+            }
+        }
+    }
+}
---
+++ samples/Nancy.RapidCache.SampleApplication.net452/Modules/Module.cs
@@ -0,0 +1,36 @@
+﻿using Nancy.RapidCache.Extensions;
+using System;
+using System.Globalization;
+
+namespace Nancy.RapidCache.SampleApplication.Net452
+{
+    public class Module : NancyModule
+    {
+        private readonly int _cachedTime = 25;
+
+        public Module()
+        {
+            Get["/"] = _ =>
+            {
+                return View["TestView.html", new { Hello = DateTime.Now.ToString(CultureInfo.InvariantCulture) }].AsCacheable(DateTime.UtcNow.AddSeconds(_cachedTime));
+            };
+
+            Get["/CachedResponse"] = _ =>
+            {
+                return Response.AsText(@"
+                this is a cached response: " + DateTime.Now.ToString(CultureInfo.InvariantCulture) + @"
+                ").AsCacheable(DateTime.UtcNow.AddSeconds(_cachedTime));
+            };
+
+            Get["/faultyResponse"] = _ =>
+            {
+                return new Response() { StatusCode = HttpStatusCode.InternalServerError }.AsCacheable(DateTime.UtcNow.AddSeconds(_cachedTime));
+            };
+
+            Get["/faultyConditionalResponse"] = _ =>
+            {
+                return new Response() { StatusCode = (string) Request.Query.fault.Value == "true" ? HttpStatusCode.InternalServerError : HttpStatusCode.OK }.AsCacheable(DateTime.UtcNow.AddSeconds(_cachedTime));
+            };
+        }
+    }
+}
