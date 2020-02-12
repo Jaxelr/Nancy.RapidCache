afbfed4a0719f3532be8ea94480c21c75937aff3
Jaxel Rojas <jaxelr@gmail.com>
2020-02-12 09:04:51 -0400

    fix: added key validation to Set operation

--- src/Nancy.RapidCache.IMemory/IMemoryCacheStore.cs
+++ src/Nancy.RapidCache.IMemory/IMemoryCacheStore.cs
@@ -7,6 +7,7 @@ namespace Nancy.RapidCache.CacheStore
     public class IMemoryCacheStore : ICacheStore
     {
         private readonly IMemoryCache cache;
+        private long sizeLimit;
 
         public IMemoryCacheStore() : this(new MemoryCache(new MemoryCacheOptions()))
         {
@@ -14,6 +15,7 @@ namespace Nancy.RapidCache.CacheStore
 
         public IMemoryCacheStore(MemoryCacheOptions options) : this(new MemoryCache(options))
         {
+            sizeLimit = options.SizeLimit ?? 0;
         }
 
         public IMemoryCacheStore(MemoryCache cache)
@@ -36,10 +38,14 @@ namespace Nancy.RapidCache.CacheStore
         public void Remove(string key) => cache.Remove(key);
 
         public void Set(string key, NancyContext context, DateTime absoluteExpiration) => Set(key, context, absoluteExpiration, 0);
-        
 
         public void Set(string key, NancyContext context, DateTime absoluteExpiration, long size)
         {
+            if (string.IsNullOrEmpty(key))
+            {
+                return;
+            }
+
             var span = absoluteExpiration - DateTime.UtcNow;
 
             if (context?.Response is Response response && span.TotalSeconds > 0)
