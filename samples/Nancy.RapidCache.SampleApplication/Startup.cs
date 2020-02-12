e891d18b2d7e9d7c8b125170dafcdbf14435b736
Jaxel Rojas <jaxelr@gmail.com>
2020-02-12 09:05:41 -0400

    fix: sample application

--- samples/Nancy.RapidCache.SampleApplication/Startup.cs
+++ samples/Nancy.RapidCache.SampleApplication/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -9,12 +10,26 @@ namespace Asp.Net.Example
 {
     public class Startup
     {
-        public void ConfigureServices(IServiceCollection services) =>
+        public void ConfigureServices(IServiceCollection services)
+        {
             services.AddLogging(opt =>
             {
                 opt.AddConsole();
                 opt.AddDebug();
             });
+
+            //For Kestrel usage
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            //For IIS usage
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+        }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
