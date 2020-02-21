490bbfc9d541c94bb551cd633cccc04636d785ec
Jaxel Rojas <jaxelr@gmail.com>
2020-02-21 15:15:58 -0400

    test: added more key generator scenarios

--- tests/Unit/KeyGeneratorFixtures.cs
+++ tests/Unit/KeyGeneratorFixtures.cs
@@ -15,6 +15,8 @@ namespace Nancy.RapidCache.Tests.Unit
         private const string QUERY = "?FakeQuery=FakeValue";
         private const string FORM = "FakeForm";
         private const string FORM_VALUE = "FakeFormValue";
+        private const string DISABLED = "&RapidCacheDisabled=true";
+        private const string REMOVE = "&RapidCacheRemove=true";
         private readonly Dictionary<string, IEnumerable<string>> acceptHeader = new Dictionary<string, IEnumerable<string>>();
 
         public KeyGeneratorFixtures()
@@ -69,7 +71,7 @@ namespace Nancy.RapidCache.Tests.Unit
                     headers: acceptHeader,
                     body: null,
                     protocol: PROTOCOL,
-                    query: string.Concat(QUERY, "&RapidCacheDisabled=true"));
+                    query: string.Concat(QUERY, DISABLED));
 
             //Act
             string key = keyGen.Get(request);
@@ -91,7 +93,7 @@ namespace Nancy.RapidCache.Tests.Unit
                     headers: acceptHeader,
                     body: null,
                     protocol: PROTOCOL,
-                    query: string.Concat(QUERY, "&RapidCacheDisabled=true"));
+                    query: string.Concat(QUERY, DISABLED));
 
             //Act
             string key = keyGen.Get(request);
@@ -113,7 +115,7 @@ namespace Nancy.RapidCache.Tests.Unit
                     headers: acceptHeader,
                     body: null,
                     protocol: PROTOCOL,
-                    query: string.Concat(QUERY, "&RapidCacheRemove=true"));
+                    query: string.Concat(QUERY, REMOVE));
 
             //Act
             string key = keyGen.Get(request);
@@ -136,7 +138,29 @@ namespace Nancy.RapidCache.Tests.Unit
                     headers: acceptHeader,
                     body: null,
                     protocol: PROTOCOL,
-                    query: string.Concat(QUERY, "&RapidCacheRemove=true"));
+                    query: string.Concat(QUERY, REMOVE));
+
+            //Act
+            string key = keyGen.Get(request);
+
+            //Assert
+            Assert.Contains(ACCEPT, key);
+            Assert.Contains(QUERY, key);
+            Assert.Contains(PATH, key);
+        }
+
+        [Fact]
+        public void Default_keyed_by_url_only_with_disable_and_remove_key_with_query()
+        {
+            //Arrange
+            var keyGen = new DefaultCacheKeyGenerator(new string[] { nameof(QUERY), nameof(ACCEPT) });
+            var request = new FakeRequest(
+                    method: METHOD,
+                    path: PATH,
+                    headers: acceptHeader,
+                    body: null,
+                    protocol: PROTOCOL,
+                    query: string.Concat(QUERY, DISABLED,  REMOVE));
 
             //Act
             string key = keyGen.Get(request);
@@ -188,6 +212,28 @@ namespace Nancy.RapidCache.Tests.Unit
             //Assert
             Assert.DoesNotContain(ACCEPT, key);
             Assert.Contains(QUERY, key);
+            Assert.Contains(PATH, key);
+        }
+
+        [Fact]
+        public void Default_keyed_by_query_with_another_param()
+        {
+            //Arrange
+            var keyGen = new DefaultCacheKeyGenerator(new string[] { nameof(ACCEPT) });
+            var request = new FakeRequest(
+                    method: METHOD,
+                    path: PATH,
+                    headers: acceptHeader,
+                    body: null,
+                    protocol: PROTOCOL,
+                    query: QUERY);
+
+            //Act
+            string key = keyGen.Get(request);
+
+            //Assert
+            Assert.Contains(ACCEPT, key);
+            Assert.DoesNotContain(QUERY, key);
             Assert.Contains(PATH, key);
         }
 
@@ -343,6 +389,34 @@ namespace Nancy.RapidCache.Tests.Unit
             Assert.Contains(FORM_VALUE, key);
             Assert.Contains(PATH, key);
         }
+
+        [Fact]
+        public void Default_keyed_by_form_header_query_without_form_key()
+        {
+            //Arrange
+            var keyGen = new DefaultCacheKeyGenerator(new string[] { nameof(QUERY), nameof(ACCEPT) });
+            var request = new FakeRequest(
+                    method: METHOD,
+                    path: PATH,
+                    headers: acceptHeader,
+                    body: null,
+                    protocol: PROTOCOL,
+                    query: QUERY);
+
+            request.Form[FORM] = FORM_VALUE;
+
+            //Act
+            string key = keyGen.Get(request);
+
+            //Assert
+            Assert.Contains(ACCEPT, key);
+            Assert.Contains(ACCEPT_VALUE, key);
+            Assert.Contains(QUERY, key);
+            Assert.DoesNotContain(FORM, key);
+            Assert.DoesNotContain(FORM_VALUE, key);
+            Assert.Contains(PATH, key);
+        }
+
 
         [Fact]
         public void UrlHashKey_Keyed_by_url()
